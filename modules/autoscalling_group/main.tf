7fb3de0724ebb7ddf1ec532266ce970e65e349c9
Darshil3838 <ec2-user@ip-172-31-25-227.ec2.internal>
2022-12-09 06:00:09 +0000

    autoscaling policy to cutdown instance added

--- modules/autoscalling_group/main.tf
+++ modules/autoscalling_group/main.tf
@@ -52,3 +52,39 @@ resource "aws_autoscaling_group" "asg_bar" {
     propagate_at_launch = true
   }
 }
+
+
+
+
+
+
+#Creating Scaling policy for AutoScaling Groupc combined CPU usage of all the instances  less then  or equal 5% 
+resource "aws_autoscaling_policy" "asg_policy5_down" {
+  name                   = "${local.name_prefix}-asg_policy_down"
+  autoscaling_group_name = aws_autoscaling_group.asg_bar.name
+  adjustment_type        = "ChangeInCapacity"
+  scaling_adjustment     = -1
+  cooldown               = 300
+}
+
+
+
+
+#Creating an alarm metric when combined CPU usage of all the instances  less then  or equal 5% 
+resource "aws_cloudwatch_metric_alarm" "metric_asg_policy_down" {
+  alarm_description   = "CPU usage of all the instances  less then  or equal 5% "
+  alarm_actions       = [aws_autoscaling_policy.asg_policy5_down.arn]
+  alarm_name          = "${local.name_prefix}_scale_down"
+  comparison_operator = "LessThanOrEqualToThreshold"
+  namespace           = "AWS/EC2"
+  metric_name         = "CPUUtilization"
+  threshold           = "5"
+  evaluation_periods  = "2"
+  period              = "120"
+  statistic           = "Average"
+
+  dimensions = {
+    AutoScalingGroupName = aws_autoscaling_group.asg_bar.name
+  }
+}
+
