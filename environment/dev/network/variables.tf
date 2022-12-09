1cc360b387ed5fd289a4f54f6b2bfc48e6f04548
EC2 Default User <ec2-user@ip-172-31-51-37.ec2.internal>
2022-12-09 00:32:19 +0000

    Adding cidr range, public & private subnet in vpc

--- environment/dev/network/variables.tf
+++ environment/dev/network/variables.tf
@@ -22,3 +22,30 @@ variable "env" {
   type        = string
   description = "dev environment"
 }
+
+
+# VPC CIDR range
+variable "vpc_cidr" {
+  default     = "10.100.0.0/16"
+  type        = string
+  description = "VPC for Dev environment"
+}
+
+
+
+
+# Provision public subnets in VPC
+variable "public_subnet_cidrs" {
+  default     = ["10.100.1.0/24", "10.100.2.0/24", "10.100.3.0/24"]
+  type        = list(string)
+  description = "Public Subnet CIDRs"
+}
+
+
+
+# Provision private subnets in VPC
+variable "private_subnet_cidrs" {
+  default     = ["10.100.4.0/24", "10.100.5.0/24", "10.100.6.0/24"]
+  type        = list(string)
+  description = "Private Subnet CIDRs"
+}
