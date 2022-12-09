3a14ddde7636502b9afc1236751d1b3913e17175
Bishal <bbhatta@myseneca.ca>
2022-12-09 14:37:14 +0000

    Bastion security group added to SG group module

--- environment/modules/sg_group/main.tf
+++ environment/modules/sg_group/main.tf
@@ -68,3 +68,36 @@ resource "aws_security_group" "lb_sg" {
     }
   )
 }
+
+
+
+# Security Group for Bastion VM
+resource "aws_security_group" "sg_bastion" {
+  name        = "allow_ssh_bastion"
+  description = "Allow SSH inbound traffic"
+  vpc_id      = data.terraform_remote_state.network.outputs.vpc_id
+
+  ingress {
+    description      = "SSH from admin"
+    from_port        = 22
+    to_port          = 22
+    protocol         = "tcp"
+    cidr_blocks      = ["${var.cloud_public_ip}/32", "${var.cloud_private_ip}/32"]
+
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  egress {
+    from_port        = 0
+    to_port          = 0
+    protocol         = "-1"
+    cidr_blocks      = ["0.0.0.0/0"]
+    ipv6_cidr_blocks = ["::/0"]
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Sg-Bastion"
+    }
+  )
+}
--- environment/modules/sg_group/variables.tf
+++ environment/modules/sg_group/variables.tf
@@ -16,7 +16,20 @@ variable "prefix" {
 }
 
 
+#private ip for cloud9
+variable "cloud_private_ip"{
+  default      = "172.31.76.50"
+  type          =string
+  description = "PRIVATE IP OF CLOUD9"
+}
 
+
+#private ip for cloud9
+variable "cloud_public_ip"{
+  default      = "3.238.228.167"
+  type          =string
+  description = "public IP OF CLOUD9"
+}
 
 
 
