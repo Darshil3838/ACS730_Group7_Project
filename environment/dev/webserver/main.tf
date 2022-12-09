7b5e10949ebd38bee61ceeb47a48f773627078e6
EC2 Default User <ec2-user@ip-172-31-52-232.ec2.internal>
2022-12-09 01:41:26 +0000

    adding more code in main.tf file in webserver

--- environment/dev/webserver/main.tf
+++ environment/dev/webserver/main.tf
@@ -42,3 +42,31 @@ data "terraform_remote_state" "network" {
     region = "us-east-1"
   }
 }
+
+
+# Bastion host VM   
+resource "aws_instance" "bastion" {
+  ami                         = data.aws_ami.latest_amazon_linux.id
+  instance_type               = var.instance_type
+  key_name                    = aws_key_pair.web_key.key_name
+  subnet_id                   = data.terraform_remote_state.network.outputs.public_subnet_id[0]
+  security_groups             = [aws_security_group.sg_bastion.id]
+  associate_public_ip_address = true
+
+  lifecycle {
+    create_before_destroy = true
+  }
+
+  tags = merge(local.default_tags,
+    {
+      "Name" = "${local.name_prefix}-Bastion"
+    }
+  )
+}
+
+
+# Adding SSH key to Amazon EC2   ccccc
+resource "aws_key_pair" "web_key" {
+  key_name   = local.name_prefix
+  public_key = file("${local.name_prefix}.pub")
+}
